e2c3669af54e97e84cc27b20a8e4a6d0eaf1fef1
PipatpongS <harak2549@gmail.com>
2026-07-26 02:32:06 +0700

    fix: disable profile edit while keeping walkin registration open

--- apps/student-reg/src/config/timeConfig.js
+++ apps/student-reg/src/config/timeConfig.js
@@ -3,11 +3,11 @@
 
 export const REGISTRATION_START_DATE = '2020-01-01T00:00:00+07:00';
 export const REGISTRATION_END_DATE = '2030-12-31T23:59:59+07:00';
-export const EDIT_DEADLINE = '2030-12-31T23:59:59+07:00';
+export const EDIT_DEADLINE = '2026-07-25T00:00:00+07:00';
 
 export const STAFF_REGISTRATION_START_DATE = '2020-01-01T00:00:00+07:00';
 export const STAFF_REGISTRATION_END_DATE = '2030-12-31T23:59:59+07:00';
-export const STAFF_EDIT_DEADLINE = '2030-12-31T23:59:59+07:00';
+export const STAFF_EDIT_DEADLINE = '2026-07-25T00:00:00+07:00';
 
 export const isRegistrationOpen = () => true;
 export const isStaffRegistrationOpen = () => true;
@@ -18,5 +18,5 @@ export const isBeforeStaffRegistration = () => false;
 export const isAfterRegistration = () => false;
 export const isAfterStaffRegistration = () => false;
 
-export const isEditClosed = () => false;
-export const isStaffEditClosed = () => false;
+export const isEditClosed = () => true;
+export const isStaffEditClosed = () => true;
